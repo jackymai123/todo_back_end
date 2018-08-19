b04b64d605b45ef574ae1fe3ab2b6be880f10d2e
JackyMai <jia.mai1@amethodschools.org>
2018-08-19 21:07:31 +0000

    everything so far

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
   get 'todo/show', to: 'todo#show'
-  get 'todo/create', to: 'todo#create'
+  get 'todo/new', to: 'todo#new'
 end
